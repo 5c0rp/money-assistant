e32a385b6a6d82e9d100b15182f5d6fdfa4eb9f8
5c0rp <tereshchenko.srg@gmail.com>
2022-05-21 13:17:01 +0300

    test

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.api_v1.api import api_router
 
-
 app = FastAPI(
     debug=settings.DEBUG,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
