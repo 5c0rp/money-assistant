518a0334a1f512e3abdde69a3922eb243f63ff60
5c0rp <tereshchenko.srg@gmail.com>
2022-05-21 18:58:39 +0300

    fix

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -7,7 +7,7 @@ from app.api.api_v1.api import api_router
 app = FastAPI(
     debug=settings.DEBUG,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    title='Money Assistant'
+    title='Money Assistant',
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
